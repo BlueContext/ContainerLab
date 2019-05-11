492c72c6effa78d74f6b5a079e5c8dce1f3aaed5
Kevin Baker <kevin.w.baker@live.com>
2019-05-11 17:51:02 -0500

    added safe mode admin password to new-dc-new-domain

--- artifacts/new-dc-new-domain/new-dc-new-domain.ps1
+++ artifacts/new-dc-new-domain/new-dc-new-domain.ps1
@@ -4,7 +4,8 @@ param(
     $domainName = "container.lab",
     $domainNetbiosName = "CONTAINERLAB",
     $logPath = "C:\Windows\NTDS",
-    $sysvolPath = "C:\Windows\SYSVOL"
+    $sysvolPath = "C:\Windows\SYSVOL",
+    [Parameter(Mandatory=$true)]$password
 )
 
 
@@ -13,6 +14,6 @@ Install-WindowsFeature AD-DomainServices RSAT RSAT-Role-Tools RSAT-AD-Tools RSAT
 
 Import-Module ADDSDeployment
 
-Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath $databasePath -DomainMode WinThreshold -DomainName $domainName -DomainNetbiosName $domainNetbiosName -ForestMode WinThreshold -InstallDns:$true -LogPath $logPath -NoRebootOnCompletion:$false -SysVolPath $sysvolPath -Force:$true
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath $databasePath -DomainMode WinThreshold -DomainName $domainName -DomainNetbiosName $domainNetbiosName -ForestMode WinThreshold -InstallDns:$true -LogPath $logPath -NoRebootOnCompletion:$false -SysVolPath $sysvolPath -SafeModeAdministratorPassword $password -Force:$true
 
 
