54f086373222bf66c63f085cdf77c8a2a8b8029f
Kevin Baker <kevin.w.baker@live.com>
2019-05-12 00:10:48 -0500

    fixing execution policy

--- artifacts/base-new-windows-configuration/base-new-windows-configuration.ps1
+++ artifacts/base-new-windows-configuration/base-new-windows-configuration.ps1
@@ -2,7 +2,7 @@
 param(
     $domain = "container.lab"
 )
-
+Set-ExecutionPolicy  -ExecutionPolicy Unrestricted -Force
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 Set-TimeZone -Id "Central Standard Time"
 Enable-PSRemoting -Force
