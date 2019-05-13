5e5c743c639ddbcb75467a05f6b292977ac32ce2
Baker <kevin.baker@energytransfer.com>
2019-05-13 09:39:15 -0500

    trying to fix base-new-windows-configuration.ps1

--- artifacts/base-new-windows-configuration/base-new-windows-configuration.ps1
+++ artifacts/base-new-windows-configuration/base-new-windows-configuration.ps1
@@ -2,10 +2,10 @@
 param(
     $domain = "container.lab"
 )
-Set-ExecutionPolicy  -ExecutionPolicy Unrestricted -Force
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-Set-TimeZone -Id "Central Standard Time"
-Enable-PSRemoting -Force
-Install-PackageProvider -Name NuGet -Force
+Set-ExecutionPolicy  -ExecutionPolicy Unrestricted -Confirm:$false -Force
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False -Confirm:$false
+Set-TimeZone -Id "Central Standard Time" -Confirm:$false
+Enable-PSRemoting -Confirm:$false -Force
+Install-PackageProvider -Name NuGet -Confirm:$false -Force
 
 Restart-Computer
